e977ac4518bc716a250569563c3ce77b4ba70782
Tyler <nguyentyler97@gmail.com>
2022-12-18 11:52:08 -0800

    feat: add functional button for adding and naming new projects

--- src/elements.js
+++ src/elements.js
@@ -3,14 +3,45 @@ import * as DOMFunctions from './DOMFunctions'
 
 const mainContainer = DOMFunctions.create('div', 'main-container', '', document.body)
 
-// Projects sidebar/container on the left
-// Add a new project button
 export const sidebar = () => {
-    const container = DOMFunctions.createTitleContentContainer('sidebar-container', 'sidebar-title', 'Projects', 'sidebar-content', '', mainContainer)
-    const button = DOMFunctions.create('button', 'sidebar-button', '+ New Project', container.content)
-    button.addEventListener('click', () => {
-        
-    })
+    const sidebarMenu = (() => {
+        const container = DOMFunctions.createTitleContentContainer('sidebar-container', 'sidebar-title', 'Projects', 'sidebar-content', '', mainContainer)
+        const button = DOMFunctions.create('button', 'sidebar-button', '+ New Project', container.content)
+        button.addEventListener('click', () => {
+            popup(container.content).forEach(element => {
+                element.classList.add('active')
+            })
+        })
+    })()
+    const popup = (parent) => {
+        const container = DOMFunctions.create('div', 'popup-container', '', mainContainer)
+        const window = DOMFunctions.create('div', 'popup-window', '', container)
+        const input = DOMFunctions.create('input', 'popup-input', '', window)
+        const setButton = DOMFunctions.create('button', 'popup-set-button', 'Set', window)
+        const cancelButton = DOMFunctions.create('button', 'popup-cancel-button', 'Cancel', window)
+        const elementArray = [
+            container,
+            window,
+            input,
+            setButton,
+            cancelButton
+        ]
+        const closePopup = () => {
+            elementArray.forEach(element => {
+                element.classList.remove('active')
+            })
+        }
+        input.setAttribute('placeholder', 'Project Name')
+        setButton.addEventListener('click', () => {
+            DOMFunctions.create('div', 'sidebar-project', input.value, parent)
+            input.value = ''
+            closePopup()
+        })
+        cancelButton.addEventListener('click', () => {
+            closePopup()
+        })
+        return elementArray
+    }
 }
 
 // Right side contains more details on project
